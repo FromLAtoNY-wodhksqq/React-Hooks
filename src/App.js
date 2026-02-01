c54261c5220cc430e13cb7ec8641533ca8ada423
최재완 <jaewan@choejaewan-ui-MacBookAir.local>
2026-02-02 07:39:57 +0900

    useDeferredValue

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState, useEffect, useRef, useContext, useMemo, useCallback,useReducer,useId,useLayoutEffect,useTransition} from "react";
+import React, {useState, useEffect, useRef, useContext, useMemo, useCallback,useReducer,useId,useLayoutEffect,useTransition,useDeferredValue} from "react";
 
 function App() {
-    const [count, setCount] = useState(0);
-    const [list, setList] = useState([]);
-    const [tran, stran] = useTransition();
-
-    const handleClick = () => {
-        setCount(c => c + 1);
-
-        stran(() => {
-            const bigList = [];
-            for (let i = 0; i < 10000; i++) {
-                bigList.push(i);
-            }
-            setList(bigList);
-        });
-    };
+    const [text, setText] = useState("");
+    const deferredText = useDeferredValue(text);
 
     return (
         <div>
-            <button onClick={handleClick}>증가</button>
-            <p>count: {count}</p>
-            {tran && <p>로딩중</p>}
+            <input
+                value={text}
+                onChange={e => setText(e.target.value)}
+            />
+            <p>즉시 값: {text}</p>
+            <p>지연 값: {deferredText}</p>
         </div>
     );
 }
-
 export default App;
