a4bf531b1975cbbd546eb0c571ae6d543ac7c7cb
최재완 <jaewan@choejaewan-ui-MacBookAir.local>
2026-02-02 07:35:01 +0900

    useTransition

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState, useEffect, useRef, useContext, useMemo, useCallback,useReducer,useId} from "react";
+import React, {useState, useEffect, useRef, useContext, useMemo, useCallback,useReducer,useId,useLayoutEffect,useTransition} from "react";
 
 function App() {
-    const id = useId();
+    const [count, setCount] = useState(0);
+    const [list, setList] = useState([]);
+    const [tran, stran] = useTransition();
 
-    return(
+    const handleClick = () => {
+        setCount(c => c + 1);
+
+        stran(() => {
+            const bigList = [];
+            for (let i = 0; i < 10000; i++) {
+                bigList.push(i);
+            }
+            setList(bigList);
+        });
+    };
+
+    return (
         <div>
-        <label htmlFor="name">이름</label>
-        <input id="name"/>
+            <button onClick={handleClick}>증가</button>
+            <p>count: {count}</p>
+            {tran && <p>로딩중</p>}
         </div>
-    )
+    );
 }
-
 
 export default App;
