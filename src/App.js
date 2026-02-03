bb24fd5a57a167399ddb9b49c96e42d4fd4fa16b
최재완 <jaewan@choejaewan-ui-MacBookAir.local>
2026-02-03 15:52:07 +0900

    useLayoutEffect

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState, useEffect, useRef, useContext, useMemo, useCallback,useReducer,useId,useLayoutEffect,useTransition,useDeferredValue,useImperativeHandle,forwardRef} from "react";
-
-const Input = forwardRef((props, ref) => {
-    const inputRef = useRef();
-
-    useImperativeHandle(ref, () => ({
-        focus() {
-            inputRef.current.focus();
-        }
-    }));
-
-    return <input ref={inputRef} />;
-});
+import React, {useState, useEffect, useRef, useContext, useMemo, useCallback,useReducer,useId,useLayoutEffect} from "react";
 
 function App() {
-    const ref = useRef();
+    const[hour,setHour] = useState(0);
+    const[minute,setMinute] = useState(0);
+    const[message,setMessage] = useState("");
 
+    useLayoutEffect(() => {
+        setMessage("Time changed!!");
+
+        const timer = setTimeout(() => {
+            setMessage("");
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, [hour, minute]);
+
+    const hourChange = (e) => {
+        let newHour;
+
+        if(hour >=23){
+            newHour = 0;
+        }
+        else {
+            newHour = hour + 1;
+        }
+
+        setHour(newHour);
+
+    };
+
+    const minuteChanged=()=>{
+        let newMinute;
+
+        if(minute >=59){
+            newMinute = 0;
+        }
+        else{
+            newMinute = minute+1;
+        }
+
+        setMinute(newMinute);
+    }
     return (
         <div>
-            <Input ref={ref} />
-            <button onClick={() => ref.current.focus()}>
-                포커스
-            </button>
+            <span>현재 시각:{hour}시 {minute}분</span>
+            <button onClick={hourChange}>Hour</button>
+            <button onClick={minuteChanged}>Minute</button>
+            <div>{message}</div>
         </div>
+
     );
 }
+
 export default App;
